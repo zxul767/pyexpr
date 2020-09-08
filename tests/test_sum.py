2c250d77f4fe78377f22eddc700b17df9e268723
zxul767 <willebaldo.gomez@gmail.com>
2020-09-08 17:35:14 -0500

    Simplify `test_sum` tests

--- tests/test_sum.py
+++ tests/test_sum.py
@@ -1,25 +1,24 @@
 from src.expression import Number, Variable, Sum
 
 
-def test__can_sum_numbers_and_variables():
-    assert Number(1) + Variable("x") == Sum(Number(1), Variable("x"))
-    assert Variable("x") + Number(1) == Sum(Variable("x"), Number(1))
+def test__can_sum_numbers_and_variables(x):
+    assert Number(1) + x == Sum(Number(1), Variable("x"))
+    assert x + Number(1) == Sum(Variable("x"), Number(1))
 
 
-def test__can_do_implicit_numeric_cast_in_sum_expressions():
-    assert 1 + Variable("x") == Sum(Number(1), Variable("x"))
-    assert Variable("x") + 1 == Sum(Variable("x"), Number(1))
+def test__can_do_implicit_numeric_cast_in_sum_expressions(x):
+    assert 1 + x == Sum(Number(1), Variable("x"))
+    assert x + 1 == Sum(Variable("x"), Number(1))
 
 
-def test__order_of_terms_should_not_matter_in_sum_comparison():
-    assert Number(1) + Variable("x") == Variable("x") + Number(1)
+def test__order_of_terms_should_not_matter_in_sum_comparison(x):
+    assert 1 + x == x + 1
 
 
-def test_can_fold_numbers_in_sum_regardless_of_position():
-    assert Number(2) + Variable("x") + Number(3) == Sum(Number(5), Variable("x"))
-    assert 2 + Variable("x") + 3 == Sum(Number(5), Variable("x"))
+def test_can_fold_numbers_in_sum_regardless_of_position(x):
+    assert 2 + x + 3 == Sum(Number(5), Variable("x"))
 
 
-def test__adding_zero_is_no_op():
-    assert 0 + Variable("x") == Variable("x")
-    assert Variable("x") + 0 == Variable("x")
+def test__adding_zero_is_no_op(x):
+    assert 0 + x == Variable("x")
+    assert x + 0 == Variable("x")
